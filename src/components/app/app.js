f00f62e8b5b321bd989fbb4b3eac1ecdd082f6aa
TedtheDev <tylerjedmunds@gmail.com>
2018-04-14 17:30:13 -0500

    refactored app

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import styled from 'styled-components';
 
 import SearchBar from '../SearchBar/search_bar';
 
+const AppDiv = styled.div`
+    width:  100%;
+    height: 100%;
+    display: grid;
+    grid-template: 10vh auto 10vh / 10vw auto 10vw;
+    grid-template-areas: 
+        "header header header"
+        ". app ."
+        "footer footer footer";
+`;
+
 const App = props => {
-    return [
-        <div>Hi from app</div>,
-        <SearchBar />
-    ]
+    return (
+        <Router>
+            <AppDiv>
+                <div>Hi from app</div>
+                <Switch>
+                    <Route exact path="/" component={SearchBar} />
+                </Switch>
+            </AppDiv>
+        </Router>
+    );
 };
 
 export default App;
