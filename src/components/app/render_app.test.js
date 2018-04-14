d1e5ca697039bf004d5c5e3775f3830cc794909b
TedtheDev <tylerjedmunds@gmail.com>
2018-04-14 17:17:26 -0500

    fixed typo on renderer

--- src/components/app/render_app.test.js
+++ src/components/app/render_app.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import App from './app';
-import rednerer from 'react-test-renderer';
+import renderer from 'react-test-renderer';
 
 describe('<App />', () => {
     test('should render correctly', () => {
-        const component = rednerer.create(
+        const component = renderer.create(
             <App />
         );
 
