4deebc28de3ec9bf83d4f836b1fd86ef40139d7e
TedtheDev <tylerjedmunds@gmail.com>
2018-04-14 17:21:54 -0500

    added SearchBar to App

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import SearchBar from '../SearchBar/search_bar';
+
 const App = props => {
-    return (
-        <div>Hi from app</div>
-    )
+    return [
+        <div>Hi from app</div>,
+        <SearchBar />
+    ]
 };
 
 export default App;
