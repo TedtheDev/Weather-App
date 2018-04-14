e1a07d09c7d6feba1818575481f26b43060ca676
TedtheDev <tylerjedmunds@gmail.com>
2018-04-14 16:46:32 -0500

    project setup

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const PORT = process.env.PORT || 8888;
+
+app.get('/', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'dist/index.html'));
+});
+
+app.get('/bundle.js', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'dist/bundle.js'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+        historyApiFallback: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            }
+        ]
+    }
+}
