3975d1fd759266507151bc59e659a7a152ade9eb
TedtheDev <tylerjedmunds@gmail.com>
2018-04-15 00:12:52 -0500

    moved searchbar component

--- src/components/SearchBar/search_bar.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import SearchBar from './search_bar';
-
-describe('<SearchBar />', () => {
-    test('should be able to type in a city', () => {
-        
-    });
-});
