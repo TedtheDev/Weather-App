119a58108700a29a20d820e70b080a55b407f167
TedtheDev <tylerjedmunds@gmail.com>
2018-04-17 00:10:26 -0500

    imported recharts; refactored styling; prepare data for recharts

--- src/components/ForecastTiles/ForecastTile/forecast_tile.js
+++ src/components/ForecastTiles/ForecastTile/forecast_tile.js
@@ -2,26 +2,59 @@ import React from 'react';
 import PropTypes  from 'prop-types';
 import styled from 'styled-components';
 
+import { LineChart, Line, XAxis, YAxis } from 'recharts';
 
 const Tile = styled.div`
-    border: 2px solid black;
+    border: 5px solid #009494;
+    border-radius: 10%;
+    box-shadow: 1px 1px 5px #454445;
     margin: 2vh 2vw;
-    max-width: 250px;
+    min-width: 250px;
+`;
+
+const DateDiv = styled.div`
+    display: flex;
+    justify-content: center;
+`;
+
+const Temp = styled.div`
+    display: flex;
+    justify-content: center;
 `;
 
 const ForecastTile = props => {
-    const date = new Date(Number(props.day.dt)*1000).toString();
+    let date = new Date(Number(props.day.dt)*1000);
+    date = `${date.toLocaleString('en-us', { month: "long" })} ${date.getDate()}`;
+    const temperatureData = [
+        {
+            name: 'Morning', temp: props.day.temp.morn
+        },
+        {
+            name: 'Day', temp: props.day.temp.day
+        },
+        {
+            name: 'Evening', temp: props.day.temp.eve
+        }, 
+        {
+            name: 'Night', temp: props.day.temp.night
+        }
+    ];
 
     return (
         <Tile>
-            <div>Temp</div>
-            <div>Date: {date}</div>
-            <div>Day: {props.day.temp.day}</div>
-            <div>Evening: {props.day.temp.eve}</div>
-            <div>Max: {props.day.temp.max}</div>
-            <div>Min: {props.day.temp.min}</div>
-            <div>Morning: {props.day.temp.morn}</div>
-            <div>Night: {props.day.temp.night}</div>
+            <DateDiv>{date}</DateDiv>
+            <Temp>Temp</Temp>
+            <div>Day: {Math.round(props.day.temp.day)} &deg;F</div>
+            <div>Evening: {Math.round(props.day.temp.eve)} &deg;F</div>
+            <div>Max: {Math.round(props.day.temp.max)} &deg;F</div>
+            <div>Min: {Math.round(props.day.temp.min)} &deg;F</div>
+            <div>Morning: {Math.round(props.day.temp.morn)} &deg;F</div>
+            <div>Night: {Math.round(props.day.temp.night)} &deg;F</div>
+            <LineChart width={200} height={100} data={temperatureData}>
+                <XAxis dataKey='name' />
+                <YAxis/>
+                <Line type="monotone" dataKey="temp" stroke="#FF5729"/>
+            </LineChart>
         </Tile>
     )
 };
