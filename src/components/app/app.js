ae65a9d28968a4d1071825d26ece63ac4cccf810
TedtheDev <tylerjedmunds@gmail.com>
2018-04-17 00:08:55 -0500

    imported Footer into App; refactored styling

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,17 +6,18 @@ import styled from 'styled-components';
 
 import Header from '../Header/header';
 import ForecastContainer from '../ForecastContainer/forecast_container';
+import Footer from '../Footer/footer';
 
 const AppDiv = styled.div`
     width:  100%;
     height: 100%;
     display: grid;
-    grid-template: 10vh auto 10vh / 10vw auto 10vw;
+    grid-template: 10% auto 10% / 10% auto 10%;
     grid-template-areas: 
         "header header header"
         ". app ."
         "footer footer footer";
-    background: #e2edff;
+        background: #F5F2DC;
 `;
 
 class App extends Component {
@@ -40,6 +41,7 @@ class App extends Component {
                     <Switch>
                         <Route exact path="/" render={() => <ForecastContainer />} />
                     </Switch>
+                    <Footer />
                 </AppDiv>
             </Router>
         );
