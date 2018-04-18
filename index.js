14a836e563adec731d1addff8a49373b13d141b2
TedtheDev <tylerjedmunds@gmail.com>
2018-04-17 22:57:37 -0500

    reconfigured application to serve index.html

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 8888;
 
 app.get('/', (req,res) => {
-    res.sendFile(path.resolve(__dirname, 'dist/index.html'));
+    res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
 app.get('/bundle.js', (req,res) => {
--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,10 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        publicPath: '/'
+        publicPath: '/dist/'
     },
     devtool: 'inline-source-map',
     devServer: {
-        contentBase: './dist',
         historyApiFallback: true
     },
     module: {
