d0706a3504ee158e3430d1de98cc0faa167e5c95
TedtheDev <tylerjedmunds@gmail.com>
2018-04-18 22:34:26 -0500

    updated travis scripts

--- travis_scripts/deploy_heroku.sh
+++ travis_scripts/deploy_heroku.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
     if [ "$1" = "development" ]; then
       docker login --username="$HEROKU_LOGIN" --password="$HEROKU_API_KEY" registry.heroku.com;
-      docker build --rm=false -t --build-arg OPENWEATHERAPI_KEY="$OPENWEATHER_API_KEY" registry.heroku.com/weather-app-dev/web:latest .;
+      docker build --rm=false -t registry.heroku.com/weather-app-dev/web:latest .;
       docker push registry.heroku.com/weather-app-dev/web:latest;
     fi
 
     if [ "$1" = "production" ]; then
       docker login --username="$HEROKU_LOGIN" --password="$HEROKU_API_KEY" registry.heroku.com;
-      docker build --rm=false -t --build-arg OPENWEATHERAPI_KEY="$OPENWEATHER_API_KEY" registry.heroku.com/your-weather-forecast/web:latest .;
+      docker build --rm=false -t registry.heroku.com/your-weather-forecast/web:latest .;
       docker push registry.heroku.com/your-weather-forecast/web:latest;
     fi
